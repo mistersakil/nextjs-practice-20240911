ff9bc9ebab052789014a8e3b59f2ffe83dd633a5
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-09-20 12:24:54 +0600

    working on server side

--- src/app/api/products/[pid]/route.ts
+++ src/app/api/products/[pid]/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export let GET = (req, context) => {
-  console.log(req.nextUrl.searchParams);
-  console.log(context.params);
+  // console.log(req.nextUrl.searchParams);
+  // console.log(context.params);
   return NextResponse.json({ msg: "hello next - dynamic params" });
 };
