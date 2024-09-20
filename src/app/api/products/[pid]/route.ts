31fa3c6f2e75f5da68bbe73032b3cd01d032a8d3
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-09-21 00:56:28 +0600

    get segmented params using product id api get request

--- src/app/api/products/[pid]/route.ts
+++ src/app/api/products/[pid]/route.ts
@@ -1,7 +1,14 @@
 import { NextResponse } from "next/server";
 
 export let GET = (req, context) => {
-  // console.log(req.nextUrl.searchParams);
-  // console.log(context.params);
-  return NextResponse.json({ msg: "hello next - dynamic params" });
+  console.log(context.params);
+  const params = context.params;
+  return NextResponse.json(
+    {
+      code: 200,
+      success: true,
+      data: params,
+    },
+    { status: 200 }
+  );
 };
