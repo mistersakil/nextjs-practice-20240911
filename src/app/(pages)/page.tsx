292c07439e75138666018edfd54b32d38a993422
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-09-20 15:17:00 +0600

    use of module css

--- src/app/(pages)/page.tsx
+++ src/app/(pages)/page.tsx
@@ -1,8 +1,10 @@
+import ModuleCss from "@/components/ModuleCss";
 import React from "react";
 export default function HomePage() {
   return (
     <>
       <h1>home</h1>
+      <ModuleCss />
     </>
   );
 }
---
+++ src/components/ModuleCss.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import CustomCss from "@/css/web/Custom.module.css";
+const ModuleCss = () => {
+  return (
+    <div>
+      <h1 className={CustomCss.fontSize}>Module css</h1>
+    </div>
+  );
+};
+
+export default ModuleCss;
