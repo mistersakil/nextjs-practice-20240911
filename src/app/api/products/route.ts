dcdbf649774905edc8b584e33722355e7265d53a
Md Sakil Mahmud <sakil.diu.cse@gmail.com>
2024-09-21 00:09:55 +0600

    next products post api route practice

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,13 +1,29 @@
 import { NextResponse } from "next/server";
 
-export async function GET(req) {
+export const GET = async (req) => {
   //   console.log(req);
   //   const { searchParams } = new Headers(req.headers);
   //   console.log(searchParams);
-  console.log(req.nextUrl.searchParams);
+  // console.log(req.nextUrl.searchParams);
+  const params = await req.nextUrl.searchParams;
+  console.log(params);
   return NextResponse.json({
     code: 200,
     success: true,
-    message: "hello next",
+    data: params,
   });
-}
+};
+
+export const POST = async (req) => {
+  // let jsonData = await req.json();
+  let jsonData = await req.formData();
+  let name = jsonData.get("name");
+  let age = jsonData.get("age");
+  console.log(jsonData, name);
+
+  return NextResponse.json({
+    code: 201,
+    success: true,
+    data: { age, name },
+  });
+};
